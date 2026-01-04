1b824731c876b6414b88b5a9f76fd4fe87344bfc
João Cuculo <jv.cuculo@gmail.com>
2026-01-04 12:46:29 -0300

    Creating project and git repository

---
+++ src/db/DbIntegrityException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+
+/**
+ *
+ * @author Usuario
+ */
+public class DbIntegrityException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public DbIntegrityException(String msg) {
+        super(msg);
+    }
+}
