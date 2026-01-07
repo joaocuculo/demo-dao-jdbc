4169da6e1e7ee3a559a426c32d301339bbe01771
João Cuculo <jv.cuculo@gmail.com>
2026-01-07 20:02:24 -0300

    implementation findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
             System.out.println(obj);
         }
         
+        System.out.println("\n=== TEST 3: seller finAll ===");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+        
         
     }
     
